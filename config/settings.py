b86c2267017e42163a79be4ffdada4dc583ec14d
Abhippsa31 <146583582+Abhippsa31@users.noreply.github.com>
2025-06-29 00:39:48 +0530

    settings.py

---
+++ config/settings.py
@@ -0,0 +1,16 @@
+TICKERS = ["RELIANCE.NS", "TCS.NS", "INFY.NS"]
+
+BACKTEST_PERIOD = "6mo"  
+INTERVAL = "1d"
+RSI_BUY_THRESHOLD = 30
+RSI_SELL_THRESHOLD = 70
+DMA_SHORT = 20
+DMA_LONG = 50
+
+SHEET_NAME = "AlgoTradeLog"
+TRADE_TAB = "Trade_Log"
+SUMMARY_TAB = "Summary_PnL"
+WIN_TAB = "win_ratio"
+MODEL_TAB = "Model_Accuracy"
+
+GOOGLE_CREDS_PATH = "config/credentials.json"
